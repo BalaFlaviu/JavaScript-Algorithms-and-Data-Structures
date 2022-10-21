b22bf5185c70f513e9d401e8c04c3165591acd33
BalaFlaviu <bala_flaviu@yahoo.com>
2022-10-21 15:52:26 +0300

    add Optional-Exercisess.js

---
+++ Optional-Exercises.js
@@ -0,0 +1,23 @@
+// Exercises Frequency Counter
+
+// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
+
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+//////////////////////////////////
+// Exercises Multiple Pointers
+
+// Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+//////////////////////////////////
+// Sliding Window
+
+//Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+// Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+// Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.
+// This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+
+// Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
